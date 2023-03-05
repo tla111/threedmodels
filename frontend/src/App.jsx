1296d7c44da82ead05fc5612bffb9dfea31d19a5
Tim La <tlamarketing111@gmail.com>
2023-03-05 06:26:06 -0500

    render navbar & hero

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,14 @@ import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, Stars
 
 const App = () => {
     return (
-        <div>
-            3D Developer Portfolio
-        </div>
+        <BrowserRouter>
+            <div className='relative z-0 bg-primary'>
+                <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+                    <Navbar />
+                    <Hero />
+                </div>
+            </div>
+        </BrowserRouter>
     )
 }
 
