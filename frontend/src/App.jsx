f0dbd5c9aa6ebb99959dce35839ec831428d58e6
Tim La <tlamarketing111@gmail.com>
2023-03-05 06:09:06 -0500

    imports imports for app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { BrowserRouter } from "react-router-dom";
+
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
     return (
