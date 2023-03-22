df3eaede5cdf88f442c5c86c34b275c7dc939180
Tim La <tlamarketing111@gmail.com>
2023-03-22 08:09:43 -0400

    building layout for contact

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -22,7 +22,13 @@ const Contact = () => {
   const handleSubmit = (e) => { }
 
   return (
-    <div>Contact</div>
+    <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+      >
+      </motion.div>
+    </div>
   )
 }
 
